86079f3f2eb7e58a89e20bc594b75fc776cc650d
Mostafa Anwar <39226625+MostafAanwar@users.noreply.github.com>
2018-09-12 15:32:50 +0200

    Add files via upload

---
+++ DogYears.js
@@ -0,0 +1,13 @@
+//Creating a variable
+let myAge=22;
+let earlyYears=2;
+earlyYears*=10.5;
+//setting varaible = to myAge - 2
+let laterYears= myAge-2;
+/*Multiply the laterYears variable by 4 to calculate the number of dog years */
+laterYears*=4;
+//console.log(earlyYears)
+//console.log(laterYears)
+var myAgeInDogYears=earlyYears+laterYears;
+var myName='Mostafa'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} old in dog years`);
